4ece5c692450b6aff6ba993c662d11b663333d39
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-10-28 11:34:21 +0700

    fix: tidy things up on next

--- intuitive-indicator/src/pages/index.js
+++ intuitive-indicator/src/pages/index.js
@@ -3,23 +3,22 @@ import { Inter } from "next/font/google";
 import Car from "@/assets/car.png";
 import { useState } from "react";
 
+/* 
+  IMG WIDTH: 150, HEIGHT: 330 
+  NISSAN MARCH: 3780 MM. SCALE FACTOR: 11.45
+  VALUE JARAK DARI DEPAN REAL / 11.45
+  SCALING FACTORS BASED ON PX AND MM
+  SCALING FACTOR Y NEED TO BE CALIBRATED
+*/
 const inter = Inter({ subsets: ["latin"] });
+const SCALING_FACTOR_X = 11.45;
+const SCALING_FACTOR_Y = 1000 / 200;
 
 export default function Home() {
-  // IMG WIDTH: 150, HEIGHT: 330 
-  // NISSAN MARCH: 3780 MM. SCALE FACTOR: 11.45
-  // VALUE JARAK DARI DEPAN REAL / 11.45
-  // PX AND MM
-  // Y AXIS STILL NOT CALIBRATED
-  const SCALING_FACTOR_X = 11.45;
-  // SCALING FACTOR Y NEED TO BE CALIBRATED
-  const SCALING_FACTOR_Y = 1000 / 200;
-  console.log(SCALING_FACTOR_Y);
-
   const [lidarData, setLidarData] = useState({
     port: "COM4",
     angle: 90,
-    distance: 1000 
+    distance: 1000
   });
 
   // POSITION STILL NOT CALIBRATED, USING IDEAL POINT FROM MARCH HEIGHT
@@ -33,8 +32,8 @@ export default function Home() {
 
     const angleInRadians = (angle * Math.PI) / 180;
 
-    let x = (Math.cos(angleInRadians) * distance);
-    let y = (Math.sin(angleInRadians) * distance);
+    let x = Math.cos(angleInRadians) * distance;
+    let y = Math.sin(angleInRadians) * distance;
 
     x = x / SCALING_FACTOR_X;
     y = y / SCALING_FACTOR_Y;
@@ -61,9 +60,7 @@ export default function Home() {
       </div>
       <div className="relative flex flex-row justify-center w-full h-full pt-[10%] pb-[10%]">
         {/* LEFT DIV FOR NEXT DEV */}
-        <div className="w-[200px]">
-
-        </div>
+        <div className="w-[200px]"></div>
 
         <div>
           <Image src={Car} alt="Car" width={150} height={150} />
