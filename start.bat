db345349d9de20d2b18f5ab1b7e21c4c73cdc5c1
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-11-01 14:05:39 +0700

    feat: shell script to start all the services

---
+++ start.bat
@@ -0,0 +1,44 @@
+@echo off
+echo Starting VisionBOI...
+
+echo Installing python packages...
+pip install -r requirements.txt
+
+for /f "tokens=1,* delims==" %%A in (config.env) do (
+    if "%%A"=="PORT_LIDAR_1" set "PORT_LIDAR_1=%%B"
+    if "%%A"=="PORT_LIDAR_2" set "PORT_LIDAR_2=%%B"
+    if "%%A"=="CAMERA_ID_1" set "CAMERA_ID_1=%%B"
+    if "%%A"=="CAMERA_ID_2" set "CAMERA_ID_2=%%B"
+    if "%%A"=="BAUDRATE" set "BAUDRATE=%%B"
+)
+
+echo Loaded PORT_LIDAR_1=%PORT_LIDAR_1%
+echo Loaded PORT_LIDAR_2=%PORT_LIDAR_2%
+echo Loaded CAMERA_ID_1=%CAMERA_ID_1%
+echo Loaded CAMERA_ID_2=%CAMERA_ID_2%
+echo Loaded BAUDRATE=%BAUDRATE%
+
+pushd serial-comm
+start /b python mock-pub.py %PORT_LIDAR_1% %PORT_LIDAR_2% > NUL 2>&1
+popd
+
+echo Running helper service...
+pushd helper
+call npm install
+start /b npm run dev > NUL 2>&1
+popd
+
+echo Running intuitive indicator service...
+pushd intuitive-indicator
+call npm install
+start /b npm run dev > NUL 2>&1
+popd
+
+echo Running real-time video stitching service...
+pushd real-time-video\testing-vid-stitching
+start /b python dev-rec.py %CAMERA_ID_1% %CAMERA_ID_2% > NUL 2>&1
+popd
+
+echo All services have been started in the background!
+echo Keep this window open to keep the services running.
+pause
